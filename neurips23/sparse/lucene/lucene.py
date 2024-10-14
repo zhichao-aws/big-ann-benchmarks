d5bac2f35f8498ef8945c47f2555bcf4fb6e0d47
zhichao-aws <zhichaog@amazon.com>
2024-10-14 02:32:15 +0000

    update
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

--- neurips23/sparse/lucene/lucene.py
+++ neurips23/sparse/lucene/lucene.py
@@ -10,44 +10,52 @@ from jpype.types import *
 class LuceneEngine(BaseSparseANN):
     def __init__(self, metric, method_param):
         if not jpype.isJVMStarted():
-            jpype.startJVM(classpath=['lucene-core-9.11.1.jar','lucene-search-engine.jar'])
+            print("start jtype jvm")
+            jpype.startJVM(
+                jpype.getDefaultJVMPath(),
+                "--add-modules=jdk.incubator.vector",
+                classpath=['lucene-core-9.11.1.jar','lucene-search-engine.jar']
+            )
             from java.util import HashMap, Map
             from org.example import LuceneSearchEngine
             
         assert metric == "ip"
-        self.engine = LuceneSearchEngine()
+        self.engine = LuceneSearchEngine("ram")
+        self.name = "lucene"
 
     def fit(self, dataset):
         print("begin fit")
+        from java.util import HashMap, Map
         ds = DATASETS[dataset]()
         
-        index = nmslib.init(method='hnsw', space='negdotprod_sparse_fast', data_type=nmslib.DataType.SPARSE_VECTOR)
         N_VEC_LIMIT = 100000 # batch size
         it = ds.get_dataset_iterator(N_VEC_LIMIT)
         id_start = 0
         for d in it:
-            index.addDataPointBatch(d,np.arange(id_start,id_start+d.shape[0]))
+            sparse_vectors = []
+            for vector in d:
+                sparse_vector = dict(zip(map(str,vector.indices),vector.data))
+                sparse_vectors.append(HashMap(sparse_vector))
+            doc_ids = list(range(id_start, id_start + d.shape[0]))
+            self.engine.ingest(JArray(Map)(sparse_vectors), JArray(JInt)(doc_ids))
             id_start += d.shape[0]
-        start = time.time()
-        index.createIndex(self.index_time_params, print_progress=True)
-        end = time.time() 
-        print('Index-time parameters', self.index_time_params)
-        print('Indexing time = %f' % (end-start))
-        self.index = index
+            print("process one block")
 
     def load_index(self, dataset):
         return None
 
     def set_query_arguments(self, parameters):
-        efS = parameters
-        query_time_params = {'efSearch': efS}
-        print('Setting query-time parameters', query_time_params)
-        self.index.setQueryTimeParams(query_time_params) 
+        return
 
     def query(self, X, topK):
+        from java.util import HashMap, Map
         # N, _ = X.shape
-        nbrs = self.index.knnQueryBatch(X, k = topK, num_threads = 8)
-        res = [x[0] for x in nbrs]
+        sparse_vectors = []
+        for vector in X:
+            sparse_vector = dict(zip(map(str,vector.indices),vector.data))
+            sparse_vectors.append(HashMap(sparse_vector))
+        res = self.engine.batchSearch(sparse_vectors, topK)
+        # res = [x[0] for x in nbrs]
         self.I = np.array(res)
         
     def get_results(self):
